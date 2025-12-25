f38fffcdb67aa2b881be38dd7717975c4b065d0c
jerryw02 <132795579+jerryw02@users.noreply.github.com>
2025-12-25 17:25:46 +0800

    Update IBgDataService.java

--- app/src/main/java/com/eveningoutpost/dexdrip/IBgDataService.java
+++ app/src/main/java/com/eveningoutpost/dexdrip/IBgDataService.java
@@ -5,6 +5,7 @@ import android.os.IInterface;
 import android.os.Parcel;
 import android.os.RemoteException;
 import com.eveningoutpost.dexdrip.BgData;
+import com.eveningoutpost.dexdrip.IBgDataCallback;
 
 public interface IBgDataService extends IInterface {
     void updateBgData(BgData data) throws RemoteException;
