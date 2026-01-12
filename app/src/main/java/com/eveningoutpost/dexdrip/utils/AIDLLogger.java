45f785b97be1dd51093fa43b75e1118f274c3cba
jerryw02 <132795579+jerryw02@users.noreply.github.com>
2026-01-12 22:59:07 +0800

    Update AIDLLogger.java

--- app/src/main/java/com/eveningoutpost/dexdrip/utils/AIDLLogger.java
+++ app/src/main/java/com/eveningoutpost/dexdrip/utils/AIDLLogger.java
@@ -1,6 +1,6 @@
 package com.eveningoutpost.dexdrip.utils;
 
-import com.eveningoutpost.dexdrip.Models.UserError;
+import com.eveningoutpost.dexdrip.models.UserError;
 
 import java.text.SimpleDateFormat;
 import java.util.Date;
