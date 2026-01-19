c1583b45ff4a1d13f329871b70c874aa51791940
jerryw02 <132795579+jerryw02@users.noreply.github.com>
2026-01-19 16:41:25 +0800

    Update BgDataService.java

--- app/src/main/java/com/eveningoutpost/dexdrip/BgDataService.java
+++ app/src/main/java/com/eveningoutpost/dexdrip/BgDataService.java
@@ -4,6 +4,7 @@ import android.app.Notification;
 import android.app.NotificationChannel;
 import android.app.NotificationManager;
 import android.app.Service;
+import android.app.ActivityManager;  // 新增这个
 import android.content.Context;
 import android.content.Intent;
 import android.os.Binder;
